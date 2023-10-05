ff0013b7a0edb549c956b3c271f373d7862848ee
5060vishalkumar <5060vishalkumar@gmail.com>
2023-10-05 07:26:45 +0530

    animated_splash_screen

--- src/Splash.js
+++ src/Splash.js
@@ -1,26 +1,14 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect} from 'react';
-import * as Animatable from 'react-native-animatable';
-const Splash = ({navigation}) => {
-    useEffect(() => {
-      setTimeout(() => {
-        navigation.navigate('Home');
-      }, 2000);
-    }, []);
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+
+const Splash = () => {
   return (
-    <View
-      style={{
-        backgroundColor: 'black',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Animatable.Text
-        style={{color: 'white', fontSize: 36, fontWeight: '800'}}
-        duration={2000}
-        animation="fadeInDownBig">
-        Splash
-      </Animatable.Text>
+    <View style={{flex: 1}}>
+      <Image
+        source={require('../assets/splash.gif')}
+        style={{width: '100%', height: '100%'}}
+        resizeMode="cover"
+      />
     </View>
   );
 };
